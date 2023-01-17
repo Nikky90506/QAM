9b311182bbcb1bd539c1a9137eec6308cecabb69
Nikky90506 <>
2023-01-17 19:40:30 +0300

    fix links

--- Quick-AccessMenu2.1/QAMloader.js
+++ Quick-AccessMenu2.1/QAMloader.js
@@ -18,7 +18,7 @@ setTimeout(
 			let n = document.createElement("script");
 			n.setAttribute("language", "JavaScript");
 			n.setAttribute("crossorigin", "anonymous");
-			n.setAttribute("src", "https://tetris245.github.io/script.js?_=" + Date.now());
+			n.setAttribute("src", "https://nikky90506.github.io/QAM/Quick-AccessMenu2.1/script.js?_=" + Date.now());
 			n.onload = () => n.remove();
 			document.head.appendChild(n);
 		}
